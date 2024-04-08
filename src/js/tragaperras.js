e46f553af671b7c27888026fcce32027d926b1ba
Albert Jané Medràn <109897647+R3dH4wk44@users.noreply.github.com>
2024-04-08 14:17:25 +0000

    modificando tragaperras

--- src/js/tragaperras.js
+++ src/js/tragaperras.js
@@ -1,53 +1,71 @@
-const SLOT_ONE = document.querySelector("#slot1");
-const SLOT_TWO = document.querySelector("#slot2");
-const SLOT_THREE = document.querySelector("#slot3");
+const OPTIONS = ['🍒','🍉','🎰','♦️','💎','💸'];
 
-const constellations = {
-    0:{title:"Aries", img:"img_constellations/aries.jpg"},
-    1:{title:"Cancer", img:"img_constellations/cancer.jpg"},
-    2:{title:"Aquarius", img:"img_constellations/aquarius.jpg"}
+let state = ['','',''];
+
+const spin = () => {
+
+    const firstSlot = getFirstSlot();
+    const secondSlot = getSecondSlot();
+    const thirdSlot = getThirdSlot();
+
+    setState(firstSlot,secondSlot,thirdSlot);
+    printSlots();
+    checkForWinner();
+
+};
+
+const getFirstSlot = () => {
+
+    const randomIndex = getRandomIndex();
+    return OPTIONS[randomIndex];
+
+};
+
+const getSecondSlot = () => {
+
+    const randomIndex = getRandomIndex();
+    return OPTIONS[randomIndex];
+    
+};
+
+const getThirdSlot = () => {
+
+    const randomIndex = getRandomIndex();
+    return OPTIONS[randomIndex];
+    
+};
+
+const getRandomIndex = () => {
+
+    return Math.floor(Math.random() * OPTIONS.length);
+};
+
+const setState = (firstSlot,secondSlot,thirdSlot) => {
+
+    state[0] = firstSlot;
+    state[1] = secondSlot;
+    state[2] = thirdSlot;
+    
+};
+
+const printSlots = () =>{
+
+    document.querySelector('.imgSlot1').innerHTML = state[0];
+    document.querySelector('.imgSlot2').innerHTML = state[1];
+    document.querySelector('.imgSlot3').innerHTML = state[2];
     
 };
 
 
+const checkForWinner = () => {
+    const isPlayerAWinner = state[0] === state[1] && state[1] === state[2];
+    if(isPlayerAWinner){ 
+        printWinnerMessage();
+    }
+};
 
-const clickOnRoll = () => {
-    
-      
-    const pickedConstellations = getConstellations();    
-    showRandomAnimation();
-    printConstellation(pickedConstellations);
-    
-    
-    };
+const printWinnerMessage = () => {
 
-    const getConstellations = () => {
+    document.querySelector('#winnerMessage').classList.remove('hidden');
 
-        const randomNumbers = getThreeRandomNumbers();
-        console.log(randomNumbers);
-        return [constellations[randomNumbers[0]],constellations[randomNumbers[1]],constellations[randomNumbers[2]]];
-        
-    };
-
-    const showRandomAnimation = () => {
-        
-
-
-
-    };
-
-    const printConstellation = (constellationsToPrint) => {
-        console.log(constellationsToPrint);
-    };
-
-    const getThreeRandomNumbers = () => {
-        const number1 = Math.floor(Math.random() * 3);
-        const number2 = Math.floor(Math.random() * 3);
-        const number3 = Math.floor(Math.random() * 3);
-        if(number1 !== number2 && number1 !== number3 && number2 !== number3){ 
-            return [number1,number2,number3];
-        }
-        return getConstellations();
-    };
-
-    const link= `<img class="rounded" src='img_constellations/aquarius.jpg' width="200" height="400" alt="">`;
+};
